a2f8ef9c920905729557de3397720982f131f76d
Justin Searls <searls@gmail.com>
2014-07-28 16:27:30 -0400

    Create a week model

---
+++ app/models/week.rb
@@ -0,0 +1,26 @@
+class Week
+  attr_reader :year, :ordinal, :beginning, :end
+
+  def self.now
+    new(Time.zone.now)
+  end
+
+  def self.for(year, ordinal)
+    new(ordinal.weeks.since(Time.zone.local(year)).beginning_of_week(:sunday))
+  end
+
+  def initialize(time)
+    @beginning = time.beginning_of_week(:sunday)
+    @end = @beginning.end_of_week(:sunday)
+    @year = @beginning.year
+    @ordinal = @beginning.strftime("%U").to_i
+  end
+
+  def next
+    self.class.new(@end + 1)
+  end
+
+  def previous
+    self.class.new(@beginning - 1)
+  end
+end
