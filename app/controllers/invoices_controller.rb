226523fdc05cfb621fc4d12e0b98cf28804d67fa
Justin Searls <searls@gmail.com>
2014-08-12 16:20:01 -0400

    Force the user to send invoices from #show action
    Instead of firing off invoices on first-press, generate the invoice 
    first and allow them to be sent to harvest from the Show action page.
    
    This will let us summarize ready_to_invoice at a glance before anything
    goes to harvest

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,31 +1,41 @@
 class InvoicesController < ApplicationController
   before_filter :require_admin
+  before_action :generate_invoice_from_params, :only => [:create, :update, :show, :send_to_harvest]
+
+  def todo
+    @invoices = Invoice.todo.map(&:generate_for_harvest)
+  end
 
   def new
     @invoice = Invoice.new(Week.now.previous_invoice_week.ymd_hash)
   end
 
   def create
-    week = Week.new(Time.zone.parse(params[:invoice][:invoicing_week]))
-    project = Project.find(params[:invoice][:project_id])
-    invoice = Invoice.find_or_create_by(week.ymd_hash.merge(:project => project)).generate_for_harvest
-
-    Present::Harvest::Api.new.update_invoice!(invoice)
-
-    redirect_to invoice.active_record_invoice
+    redirect_to @invoice.active_record_invoice
   end
 
   def update
-    invoice = Invoice.find(params[:id]).generate_for_harvest
-    Present::Harvest::Api.new.update_invoice!(invoice)
-    redirect_to invoice.active_record_invoice
+    redirect_to @invoice.active_record_invoice
   end
 
   def show
-    @invoice = Invoice.find(params[:id]).generate_for_harvest
   end
 
-  def todo
-    @invoices = Invoice.todo.map(&:generate_for_harvest)
+  def send_to_harvest
+    Present::Harvest::Api.new.update_invoice!(@invoice)
+    flash[:info] = ["Sent invoice to Harvest!"]
+    redirect_to @invoice.active_record_invoice
+  end
+
+private
+
+  def generate_invoice_from_params
+    @invoice = if params[:id].present?
+      Invoice.find(params[:id]).generate_for_harvest
+    else
+      week = Week.new(Time.zone.parse(params[:invoice][:invoicing_week]))
+      project = Project.find(params[:invoice][:project_id])
+      Invoice.find_or_create_by(week.ymd_hash.merge(:project => project)).generate_for_harvest
+    end
   end
 end
