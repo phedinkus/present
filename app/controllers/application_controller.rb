453e22b9a82a00defe0df0959299fb0bdc340b95
Justin Searls <searls@gmail.com>
2014-08-13 10:44:07 -0400

    allow admin access based on who is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_admin
-    unless @current_user.admin?
+    unless @logged_in_user.admin?
       render :text => "Sorry, this feature is only available to admins!"
     end
   end
