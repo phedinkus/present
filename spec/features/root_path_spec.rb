f8759c30b5ce5d0bc6b47595a8bc3f07a22c7fb7
Justin Searls <searls@gmail.com>
2014-08-19 18:15:41 -0400

    Add a test that adds a project to the timesheet
    * Verifies that the newly added project is on top of the list
    * Verifies three projects are present (holiday, vacation + the new one)
    * Separately verifies the default state

--- spec/features/root_path_spec.rb
+++ spec/features/root_path_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'the root path of the webapp' do
-  Given!(:user) {
+  Given {
     User.create!(
       :id => 1,
       :name => "Joe Schmoe",
@@ -13,6 +13,30 @@ describe 'the root path of the webapp' do
       )
     )
   }
+  Given {
+    Project.create!(
+      :name => "Some Project",
+      :active => true,
+      :client => Client.new(:name => "Acme", :active => true)
+    )
+  }
+
   Given { visit('/') }
-  Then { expect(page).to have_content("8/17/14") }
+
+  Given(:project_rows) { all('.timesheet-form table tbody tr') }
+
+  context 'initial state' do
+    Then { project_rows.size == 2 }
+    And { project_rows[0].all('td').first.text == "Vacation" }
+    And { project_rows[1].all('td').first.text == "Holiday" }
+  end
+
+  context 'after adding a project' do
+    When { click_button "Add Project" }
+    When(:first_row_text) { project_rows[0].all('td').first.text }
+    Then { project_rows.size == 3 }
+    And { expect(first_row_text).to include("Some Project") }
+    And { expect(first_row_text).to include("Acme") }
+  end
+
 end
