554719bc9634d8d94a14b82e92d4dd49fe9d770b
Justin Searls <searls@gmail.com>
2014-08-19 17:42:54 -0400

    move helper format to make it pry friendly

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -1,10 +1,10 @@
 module WeeksHelper
   def grouped_options_for_clients_and_projects_for(timesheet)
-    clients_with_and_including_addable_projects = Client
-      .includes(:projects)
-      .where(:active => true)
-      .where.not("projects.id" => timesheet.projects)
-      .where("projects.active" => true)
+    clients_with_and_including_addable_projects = Client.
+      includes(:projects).
+      where(:active => true).
+      where.not("projects.id" => timesheet.projects).
+      where("projects.active" => true)
 
     Hash[
       clients_with_and_including_addable_projects.map do |c|
