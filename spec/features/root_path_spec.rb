6cf03701530ae60d354125282008bde781242cbf
Justin Searls <searls@gmail.com>
2014-08-19 17:04:48 -0400

    Write a failing test with a fixture User

--- spec/features/root_path_spec.rb
+++ spec/features/root_path_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe 'the root path of the webapp' do
-  Then { 1 == 1 }
+  Given!(:user) {
+    User.create!(
+      :id => 1,
+      :name => "Joe Schmoe",
+      :github_account => GithubAccount.new(
+        :github_id => 1337,
+        :login => "joe",
+        :access_token => "not-a-real-access-token",
+        :scopes => []
+      )
+    )
+  }
+  Then { expect(page).to have_content("8/17/14") }
 end
