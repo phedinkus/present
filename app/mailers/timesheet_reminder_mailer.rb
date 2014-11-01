180d87660f5998effa82d0de5f4dd1233c2f49c2
Justin Searls <searls@gmail.com>
2014-11-01 15:18:14 -0400

    Add a mailer for failed notifications

--- app/mailers/timesheet_reminder_mailer.rb
+++ app/mailers/timesheet_reminder_mailer.rb
@@ -1,8 +1,14 @@
 class TimesheetReminderMailer < ActionMailer::Base
-  default from: "finances@testdouble.com"
+  ADMIN_EMAIL = "finances@testdouble.com"
+  default from: ADMIN_EMAIL
 
   def reminder_email(user)
     @user = user
     mail(to: @user.github_account.email, subject: 'Present: Ready to invoice?')
+  end
+
+  def reminder_failed_email(user)
+    @user = user
+    mail(to: ADMIN_EMAIL, subject: "Failed to remind #{user.name}")
   end
 end
