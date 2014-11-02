bfdb381dfaf707c7ed2a0da73ab0d64eff20d36d
Justin Searls <searls@gmail.com>
2014-11-02 13:33:02 -0500

    debug heroku w/ time in rake issue

---
+++ app/models/jobs/reminder_emails.rb
@@ -0,0 +1,32 @@
+module Jobs
+  module ReminderEmails
+    def self.send(now = Time.zone.now)
+      return unless now.friday?
+      week = Week.new(now)
+      return unless week.invoice_week?
+
+      puts "YAY! I MADE IT #{Time.zone.now}"
+
+      # Job.create!(:name => "send_reminder_emails").tap do |job|
+      #   puts "Starting job #{job.id}; sending reminder e-mails for invoice week starting #{week.ymd_dash}"
+      #   timesheet_status = Answers::TimesheetStatus.status_for(week)
+      #
+      #   (timesheet_status.missing_timesheets.map(&:user) + timesheet_status.not_ready_timesheets.map(&:user)).compact.uniq.each do |(user, timesheets)|
+      #     mail = if user.github_account.email.present?
+      #       TimesheetReminderMailer.reminder_email(user).tap do |mail|
+      #         mail.deliver
+      #         puts "Reminder sent to #{user.name} (at #{user.github_account.email})"
+      #       end
+      #     else
+      #       TimesheetReminderMailer.reminder_failed_email(user).tap do |mail|
+      #         mail.deliver
+      #         puts "Reminder could not be sent to #{user.name}, notified admins instead."
+      #       end
+      #     end
+      #
+      #     Email.create!(:job => job, :mailer => "TimesheetReminderMailer", :to => mail.to.join(", "), :subject => mail.subject, :body => mail.body.to_s)
+      #   end
+      # end.update!(:finished_at => Time.zone.now)
+    end
+  end
+end
--- lib/tasks/send_reminder_emails.rake
+++ lib/tasks/send_reminder_emails.rake
@@ -1,24 +1,4 @@
 desc 'Send reminder e-mails to users'
-task :send_reminder_emails => :environment do |t, args|
-  week = Week.now.closest_invoice_week
-  Job.create!(:name => "send_reminder_emails").tap do |job|
-    puts "Starting job #{job.id}; sending reminder e-mails for invoice week starting #{week.ymd_dash}"
-    timesheet_status = Answers::TimesheetStatus.status_for(week)
-
-    (timesheet_status.missing_timesheets.map(&:user) + timesheet_status.not_ready_timesheets.map(&:user)).compact.uniq.each do |(user, timesheets)|
-      mail = if user.github_account.email.present?
-        TimesheetReminderMailer.reminder_email(user).tap do |mail|
-          mail.deliver
-          puts "Reminder sent to #{user.name} (at #{user.github_account.email})"
-        end
-      else
-        TimesheetReminderMailer.reminder_failed_email(user).tap do |mail|
-          mail.deliver
-          puts "Reminder could not be sent to #{user.name}, notified admins instead."
-        end
-      end
-
-      Email.create!(:job => job, :mailer => "TimesheetReminderMailer", :to => mail.to.join(", "), :subject => mail.subject, :body => mail.body.to_s)
-    end
-  end.update!(:finished_at => Time.zone.now)
+task :send_reminder_emails => :environment do
+  Jobs::ReminderEmails.send
 end
