175531f62592dcb9175fd49361842931555d0a1f
Justin Searls <searls@gmail.com>
2015-01-04 16:59:47 -0500

    Fixes #48

--- app/helpers/weeks_helper.rb
+++ app/helpers/weeks_helper.rb
@@ -1,10 +1,11 @@
 module WeeksHelper
   def grouped_options_for_clients_and_projects_for(timesheet)
-    clients_with_and_including_addable_projects = Client
-      .includes(:projects)
-      .where(:active => true)
-      .where.not("projects.id" => timesheet.projects)
-      .where("projects.active" => true)
+    clients_with_and_including_addable_projects = Client.
+      includes(:projects).
+      where(:active => true).
+      where.not("projects.id" => timesheet.projects).
+      where("projects.active" => true).
+      order('clients.name, projects.name')
 
     Hash[
       clients_with_and_including_addable_projects.map do |c|
